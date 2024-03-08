f482896710700d95771e1dff4aa3c71b00394afd
Mehdi-Elargoubi <mehdi.elargoubi11@gmail.com>
2024-03-08 20:36:56 +0100

    Ajouter un ListView-1

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
@@ -9,14 +11,12 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val tvHello = findViewById<TextView>(R.id.tvHello)
-
         // 1- Recuperer login envoyé par l'actìvityMain
         val login = intent.getStringExtra("login")
-
-        //2- Afficher login dans tvHello
-        tvHello.text = "Bienvenu : $login"
-
+        val listPosts = findViewById<ListView>(R.id.listPosts)
+        val postsArray = listOf("Post 1","Post 2","Post 3","Post 4")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, postsArray)
+        listPosts.adapter = adapter
 
     }
 }
